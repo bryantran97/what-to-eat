aa92da7e392336359c84f4fc86043baa34099ee8
bryantran97@gmail.com <bryantran97@gmail.com>
2017-08-19 23:13:45 -0500

    finished embedding and error handling, need to style now

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ var foodList     = require("./food-list.js");
 var app = express();
 app.set("view engine", "ejs");                  // So it'll say on EJS by default when rendering
 
+app.listen(3000, function() {
+    console.log("Server is working properly, Bryan.");
+});
+
 /* =================== */
 /*      RANDOM FOOD    */
 /* =================== */
@@ -19,9 +23,9 @@ function generateRandomFood(max){               // Generate a random food from f
     var x = Math.random() * (max - 1) + 1;
     return foodList[Math.floor(x)];
 }
-var food = generateRandomFood(foodList.length);
 
 function generateRandomNum(max){                // Generate a random restaraunt
+    var max = max-1;
     var x = Math.random() * (max);
     return Math.floor(x);
 }
@@ -29,15 +33,17 @@ function generateRandomNum(max){                // Generate a random restaraunt
 /* =================== */
 /*       ROUTING       */
 /* =================== */
+// Get the root page
 app.get("/", function(req, res){
     res.render("index");
 });
 
+// Getting results page
 app.get("/results", function(req, res){
     var location = req.query.location;
-    /* =================== */
-    /*       API REQ       */
-    /* =================== */
+    /* ~~~~~~~~~~~~~~~ */
+    /*     API REQ     */
+    /* ~~~~~~~~~~~~~~~ */
     // Input API's clientID and clientSecret
     var clientId = "-";
     var clientSecret = "-";
@@ -46,29 +52,32 @@ app.get("/results", function(req, res){
     var token = yelp.accessToken(clientId, clientSecret).then(response => {
         makeAPICall(response.jsonBody.access_token);
     }).catch(e => {
-        console.log(e);
+        res.render("error");
     });
 
     // Make API call
     function makeAPICall(token) {
+        // Allow us to use the yelp API client
         const client = yelp.client(token);
-
+        // Generate a random food every time we load
+        var food = generateRandomFood(foodList.length);
+        
+        // Search
         client.search({
             term: food,
             location: location
         }).then(response => {  
             // Generate a random restauraunt of the list of restaurants
             var num = generateRandomNum(response.jsonBody.businesses.length);
-            // Retrieve Object specific information of this restaurant
-            res.render("results", {food: food, location: location, business: response.jsonBody.businesses[num]});
-            
-            console.log(response.jsonBody.businesses[num]);
+            // If everything godes swell, find a specific restaurant and push that into the rendered page
+            if(response.jsonBody.businesses[num].name !== "undefined"){
+                var business = response.jsonBody.businesses[num];
+                res.render("results",  {food: food, location: location, business: business});  
+            } else { // If not, just send in error page
+                res.render("error");
+            }
         }).catch(e => {
-          console.log(e);
+            res.render("error");
         });
     }
-});
-
-app.listen(3000, function() {
-    console.log("Server is working properly, Bryan.");
 });
