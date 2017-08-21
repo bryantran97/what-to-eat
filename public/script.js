6d38601ec65772e7c3b8de03b126727f8bf7307c
bryantran97@gmail.com <bryantran97@gmail.com>
2017-08-21 12:54:28 -0500

    fixed UI A LOT

--- public/script.js
+++ public/script.js
@@ -1,17 +1,22 @@
-        $(document).ready(function() {// Ajax call
-            $("form").submit(function(e) {
-                var url = "/results";
-                console.log(location);
-                $.ajax({
-                    type:"GET",
-                    url: url,
-                    // seralize form elements
-                    data: $("form").serialize(),
-                    success: function(data) {
-                        document.getElementsByTagName('body')[0].innerHTML = data;
-                    }
-                });
+$(document).ready(function() { // Ajax call
 
-                e.preventDefault();
-            });
+    $('body').hide().fadeIn(1300);
+
+    $("form").submit(function(e) {
+        var url = "/results";
+        console.log(location);
+        $.ajax({
+            type: "GET",
+            url: url,
+            // seralize form elements
+            data: $("form").serialize(),
+            success: function(data) {
+
+                document.getElementsByTagName('body')[0].innerHTML = data;
+                $('body').hide().fadeIn('slow');
+            }
         });
+
+        e.preventDefault();
+    });
+});
